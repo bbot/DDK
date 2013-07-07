761d8aee8bc9ab59cc096c16711ba27027881f67
bbot <bbot@bbot.org>
2013-07-06 17:10:59 -0700

    Removed some boilerplate, added some boilerplate

--- main.js
+++ main.js
@@ -1,42 +1,32 @@
-// The main module of the sbierwagen Add-on.
+// DDK - Automatically loads high resolution tumblr images,
+//
+// blah blah explain stuff here
+//
+// This is free and unencumbered software released into the public domain.
 
-// Modules needed are `require`d, similar to CommonJS modules.
-// In this case, creating a Widget that opens a new tab needs both the
-// `widget` and the `tabs` modules.
 var Widget = require("widget").Widget;
-var tabs = require('tabs');
+var tabs = require("tabs");
+var Request = require("request").Request;
+
+var checkImageExists = Request({
+    url: "http://bbot.org/bbot.png",
+    onComplete: function (response) {
+        console.log("Sent request to"); //oops how do I put a variable in a string
+    }
+});
 
 exports.main = function() {
 
-    // Widget documentation: https://addons.mozilla.org/en-US/developers/docs/sdk/latest/modules/sdk/widget.html
-
     new Widget({
-        // Mandatory string used to identify your widget in order to
-        // save its location when the user moves it in the browser.
-        // This string has to be unique and must not be changed over time.
         id: "sbierwagen-widget-1",
-
-        // A required string description of the widget used for
-        // accessibility, title bars, and error reporting.
         label: "My Mozilla Widget",
-
-
-        // An optional string URL to content to load into the widget.
-        // This can be local content or remote content, an image or
-        // web content. Widgets must have either the content property
-        // or the contentURL property set.
-        //
-        // If the content is an image, it is automatically scaled to
-        // be 16x16 pixels.
         contentURL: "http://www.mozilla.org/favicon.ico",
 
-        // Add a function to trigger when the Widget is clicked.
         onClick: function(event) {
             
             // Tabs documentation: https://addons.mozilla.org/en-US/developers/docs/sdk/latest/modules/sdk/tabs.html
 
-            // Open a new tab in the currently active window.
-            tabs.open("http://www.mozilla.org");
+            checkImageExists.get();
 
         }
     });
